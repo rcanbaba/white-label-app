9d4f97590ef88792668af6203860a1ec5905e215
Can Babaoğlu <canbabaoglu@scorpapp.com>
2023-12-17 21:48:13 +0300

    - app created initial setups completed.

---
+++ dealroom/dealroom/dealroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  dealroomApp.swift
+//  dealroom
+//
+//  Created by Can Babaoğlu on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct dealroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
